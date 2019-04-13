b32f9c48109a1c9cfe8ed878a572b51cdec98530
にゃんこ せんせい <1187130615@qq.com>
2019-04-13 19:39:28 +0800

    init project

---
+++ app/__init__.py
@@ -0,0 +1,51 @@
+#!/Users/gkai/anaconda3/bin/python
+#coding:utf-8
+
+
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from logging.handlers import RotatingFileHandler
+import os
+import logging
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+
+from flask_babel import Babel
+from flask import request
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+bootstrap = Bootstrap(app)
+mail = Mail(app)
+from app import routes, models, errors
+
+
+moment = Moment(app)
+babel = Babel(app)
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
